6321911829771cb53362075d0424d6ad6eb4378c
mvpc-11232 <mvp2612@hotmail.com>
2022-08-20 08:45:14 +0530

    Auto Push Commit

--- sample-ec2/ec2.tf
+++ sample-ec2/ec2.tf
@@ -1,7 +1,7 @@
 # Creates EC2
 resource "aws_instance" "b49-ec2" {
   ami           = "ami-052d9bac7baef4290"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name    = "my-first-ec2"
@@ -9,5 +9,6 @@ resource "aws_instance" "b49-ec2" {
     Project = "cc"
   }
 }
+
 
 
