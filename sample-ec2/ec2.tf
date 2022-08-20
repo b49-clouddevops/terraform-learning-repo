4069f83846dc490ca66d5acc4d585fff20fef815
mvpc-11232 <mvp2612@hotmail.com>
2022-08-20 08:17:42 +0530

    Auto Push Commit

--- sample-ec2/ec2.tf
+++ sample-ec2/ec2.tf
@@ -4,7 +4,9 @@ resource "aws_instance" "b49-ec2" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "my-first-ec2"
+    Name    = "my-first-ec2"
+    ENV     = "dev"
+    Project = "cc"
   }
 }
 
