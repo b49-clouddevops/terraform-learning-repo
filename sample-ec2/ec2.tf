c947ed19aa45f728cb29b565b3c3b33759f2560c
mvpc-11232 <mvp2612@hotmail.com>
2022-08-20 08:37:17 +0530

    Auto Push Commit

--- sample-ec2/ec2.tf
+++ sample-ec2/ec2.tf
@@ -17,4 +17,4 @@ resource "aws_instance" "b49-ec2" {
 
 What is the manin moto of terraform 
 1) No Manual changes on console
-2) Every change should be versioned and will be applied only after a PR. 
+2) Every change should be versioned and will be applied only after a PR.  
