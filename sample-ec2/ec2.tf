38866cc1b14930a95d3882161fa0b9ae43b96e6a
mvpc-11232 <mvp2612@hotmail.com>
2022-08-20 08:24:09 +0530

    Auto Push Commit

--- sample-ec2/ec2.tf
+++ sample-ec2/ec2.tf
@@ -15,4 +15,6 @@ output "privateip" {
    value  = aws_instance.b49-ec2.private_ip  # private_ip is the attribute
 }
 
-In how manys ways
+In how manys ways can you authenticate to linux ?????
+1) key_name 
+2) Username & Password []
