13c347969f6253484a56cff980b3b7056805507b
mvpc-11232 <mvp2612@hotmail.com>
2022-08-23 08:45:21 +0530

    Auto Push Commit

--- robot-basic-setup/ec2/route53.tf
+++ robot-basic-setup/ec2/route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "component" {
   zone_id = "Z09626353E72G6GNQ0R5A"
-  name    = "www.example.com"
+  name    = "${var.COMPONENT}-dev.roboshop.internal"
   type    = "A"
   ttl     = 300
   records = [aws_eip.lb.public_ip]
