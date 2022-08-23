2ce1068d2baf23bea37a26fa0b2934cd813e8029
mvpc-11232 <mvp2612@hotmail.com>
2022-08-23 08:38:12 +0530

    Auto Push Commit

--- robot-basic-setup/ec2/ec2.tf
+++ robot-basic-setup/ec2/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_spot_instance_request" "cheap_worker" {
-  ami                  = data.aws_ami.base-ami.image_id
-  instance_type         = "t3.micro"
-  wait_for_fulfillmen   = true 
+  ami                    = data.aws_ami.base-ami.image_id
+  instance_type          = "t3.micro"
+  wait_for_fulfillment   = true 
   vpc_security_group_ids = [aws_security_group.allow_all.id]
 
   tags = {
