17794cedd7575ec9a25588fdc82baf148b370f20
mvpc-11232 <mvp2612@hotmail.com>
2022-08-23 07:46:17 +0530

    Auto Push Commit

--- example2/ec2/main.tf
+++ example2/ec2/main.tf
@@ -3,6 +3,8 @@ resource "aws_instance" "b49-ec2" {
   ami                     = data.aws_ami.base-ami.image_id
   instance_type           = "t3.micro"
   vpc_security_group_ids  = [var.sg]
+
+
 }
 
 variable "sg" {}
