d561f4d41c2027c28b8fd8cf5101fcf156913802
mvpc-11232 <mvp2612@hotmail.com>
2022-08-23 07:53:26 +0530

    Auto Push Commit

--- example2/ec2/main.tf
+++ example2/ec2/main.tf
@@ -5,11 +5,11 @@ resource "aws_instance" "b49-ec2" {
   vpc_security_group_ids  = [var.sg]
 
 
-  provisioner "local-exec" {
+  provisioner "local-exec" {   # To do something on the machine where you're running the terraform
     command = <<EOF 
         sleep 40 
         cd /home/centos/ansible 
-        ansible-playbook -i ${self.public_ip} roboshop-push.yml -e ENV=dev  -e COMPONENT=frontend -e TAG_NAME=0.0.2 roboshop-push.yml  
+        ansible-playbook -i ${self.public_ip} -e ENV=dev -e COMPONENT=frontend -e TAG_NAME=0.0.2 roboshop-push.yml  
     EOF 
    }
 }
